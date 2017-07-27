fed85e865e99362678ce8771be07bf80e6fc290a
Victor Oliveira <victorlaertedoliveira@gmail.com>
2017-07-27 00:08:45 -0300

    table with forecast

--- app/src/main/java/com/victorlaerte/na_onda/util/ViewUtil.java
+++ app/src/main/java/com/victorlaerte/na_onda/util/ViewUtil.java
@@ -15,13 +15,13 @@ public class ViewUtil {
 
 	public static LinearLayout createRow(Context context, int weightSum) {
 
-		int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, context.getResources().getDisplayMetrics());
+//		int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, context.getResources().getDisplayMetrics());
 
 		LinearLayout linearLayout = new LinearLayout(context);
 		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
 			LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-		layoutParams.setMargins(margin, margin, margin, margin);
-		layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+//		layoutParams.setMargins(margin, margin, margin, margin);
+		layoutParams.gravity = Gravity.CENTER;
 		linearLayout.setLayoutParams(layoutParams);
 
 		linearLayout.setWeightSum(weightSum);
@@ -30,12 +30,13 @@ public class ViewUtil {
 	}
 
 	public static TextView createTextViewCell(Context context, String text) {
-		int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, context.getResources().getDisplayMetrics());
+		int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, context.getResources().getDisplayMetrics());
 		TextView textView = new TextView(context);
 		textView.setGravity(Gravity.CENTER);
 		TableLayout.LayoutParams layoutParams = new TableLayout.LayoutParams(
 			ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
 		layoutParams.setMargins(margin, margin, margin, margin);
+		layoutParams.gravity = Gravity.CENTER;
 		textView.setLayoutParams(layoutParams);
 		textView.setText(text);
 		return textView;
